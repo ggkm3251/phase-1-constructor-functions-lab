f9ef48abf338540917b429751b52ee9aa54f601d
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-20 01:57:50 +0300

    commit tree 21beaa1469ae5584bc3fde56d452d35118771316

---
+++ index.js
@@ -0,0 +1,19 @@
+function Scooter(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model
+}
+samson.year;
+
+function Driver(name, age, experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+allison.name;
+
+function PickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+}
+poshHotel.city;
